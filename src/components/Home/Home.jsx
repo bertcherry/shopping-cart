b3ccf5fa0ae08af2bf97a61f239785bfc4397122
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-09-06 13:15:54 -0700

    style text overlay

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,20 @@
 import styles from "./Home.module.css";
 import Nav from "../Nav/Nav";
 import clothing from "../../assets/Clothing.jpg";
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <>
             <Nav />
-            <div className={styles.container}><img src={clothing} alt="A rack of clothes seen through a window"/></div>
+            <div className={styles.hero}>
+                <div className={styles.container}><img src={clothing} alt="A rack of clothes seen through a window"/></div>
+                <div className={styles.overlay}>
+                    <p>Couture and electronics for the modern life</p>
+                    <button><Link to="shop">Shop</Link></button>
+                </div>
+                
+            </div>
         </>
         
     );
