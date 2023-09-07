8e0c59ee4f98c99a718cf06c78ddc8b4ab061a80
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-09-07 09:49:13 -0700

    style input

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -10,7 +10,7 @@ function Card({ id, title, price, imgsrc, addItem }) {
         <div className={styles.card}>
             <div className={styles.container}><img src={imgsrc}/></div>
             <h2>{title}</h2>
-            <div>${price}</div>
+            <div>${price.toFixed(2)}</div>
             <form className={styles.interactive}>
                 <label>Quantity: <input name="quantity" type="number" min="0" id={id}/></label>
                 <button type="submit" className={styles.add} onClick={(e) => handleClick(e, id)}>Add</button>
